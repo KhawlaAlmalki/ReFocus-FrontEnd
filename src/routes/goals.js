b3b505399ddbee1ac7a2670034b671270eb48102
KhawlaAlmalki <s202247320@kfupm.edu.sa>
2025-12-01 13:31:54 +0300

    Controllers

--- src/routes/goals.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { setGoal, getGoal } from "../controllers/goalController.js";
-
-const router = express.Router();
-
-router.post("/", setGoal);
-router.get("/:userId", getGoal);
-
-export default router;
--- src/routes/survey.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { submitSurvey, getSurveyByUser } from "../controllers/surveyController.js";
-
-const router = express.Router();
-
-router.post("/", submitSurvey);
-router.get("/:userId", getSurveyByUser);
-
-export default router;
